549e1254bd9e29decf0d4b8420f3935282772ca3
Никита <iY37k4hZ>
2017-01-01 23:02:55 +0300

    Some refactor

--- Dynamics/main.cpp
+++ Dynamics/main.cpp
@@ -4,21 +4,16 @@
 #include <string>
 using namespace std;
 
-const double lattice_constant = 3.615;
-const double cutoff = 1.7 * lattice_constant;
-
-/*
 //Cu
-const double A_1 = 0;
+/*const double A_1 = 0;
 const double A_0 = 0.0855;
 const double s = 1.2240;
 const double p = 10.96;
 const double q = 2.278;
-const double r0 = lattice_constant / sqrt(2);
-*/
-/*
+const double r0 = lattice_constant / sqrt(2);*/
+
 //Ag
-const double lattice_constant = 4.085;
+/*const double lattice_constant = 4.085;
 const double cutoff = 1.7 * lattice_constant;
 const double A_1 = 0;
 const double A_0 = 0.1028;
@@ -26,8 +21,6 @@ const double s = 1.178;
 const double p = 10.928;
 const double q = 3.139;
 const double r0 = lattice_constant / sqrt(2);*/
-
-const double qsi = 0.8018993929636421;
 
 class Atom {
 public:
@@ -49,6 +42,83 @@ public:
 	double r0 = 0;
 };
 
+class Translation {
+public:
+	double translation[9];
+	double lattice_constant = 0;
+	double alpha = 0;
+	void make_it_pure(){
+		//x
+		translation[0] = 3 * lattice_constant;
+		translation[1] = 0;
+		translation[2] = 0;
+		//y
+		translation[3] = 0;
+		translation[4] = 3 * lattice_constant;
+		translation[5] = 0;
+		//z
+		translation[6] = 0;
+		translation[7] = 0;
+		translation[8] = 3 * lattice_constant;
+	}
+	void make_it_B() {
+		//x
+		translation[0] = 3 * lattice_constant * (1 + alpha);
+		translation[1] = 0;
+		translation[2] = 0;
+		//y
+		translation[3] = 0;
+		translation[4] = 3 * lattice_constant * (1 + alpha);
+		translation[5] = 0;
+		//z
+		translation[6] = 0;
+		translation[7] = 0;
+		translation[8] = 3 * lattice_constant * (1 + alpha);
+	}
+	void make_it_C11() {
+		//x
+		translation[0] = 3 * lattice_constant * (1 + alpha);
+		translation[1] = 0;
+		translation[2] = 0;
+		//y
+		translation[3] = 0;
+		translation[4] = 3 * lattice_constant * (1 + alpha);
+		translation[5] = 0;
+		//z
+		translation[6] = 0;
+		translation[7] = 0;
+		translation[8] = 3 * lattice_constant;
+	}
+	void make_it_C12() {
+		//x
+		translation[0] = 3 * lattice_constant * (1 + alpha);
+		translation[1] = 0;
+		translation[2] = 0;
+		//y
+		translation[3] = 0;
+		translation[4] = 3 * lattice_constant * (1 - alpha);
+		translation[5] = 0;
+		//z
+		translation[6] = 0;
+		translation[7] = 0;
+		translation[8] = 3 * lattice_constant;
+	}
+	void make_it_C44() {
+		//x
+		translation[0] = 3 * lattice_constant;
+		translation[1] = alpha * 3 * lattice_constant;
+		translation[2] = 0;
+		//y
+		translation[3] = alpha * 3 * lattice_constant;
+		translation[4] = 3 * lattice_constant;
+		translation[5] = 0;
+		//z
+		translation[6] = 0;
+		translation[7] = 0;
+		translation[8] = 3 * lattice_constant / (1 - alpha * alpha);
+	}
+};
+
 bool toofar(Atom part, Atom part2, double cutoff) {
 	if (part.x == part2.x && part.y == part2.y && part.z == part2.z)
 		return true;
@@ -57,7 +127,7 @@ bool toofar(Atom part, Atom part2, double cutoff) {
 		+ (part2.z - part.z) * (part2.z - part.z)) > cutoff ? true : false;
 }
 
-double full_energy(vector <vector<Atom>> &contains, int n_x, int n_y, int n_z, RGL potentials[3], bool impurity, double translation[9]) {
+double full_energy(vector <vector<Atom>> &contains, int n_x, int n_y, int n_z, double cutoff, RGL potentials[3], double translation[9], bool surface) {
 
 	double E_full = 0;
 	double bulk_size_x = translation[0] + translation[3] + translation[6];
@@ -102,16 +172,18 @@ double full_energy(vector <vector<Atom>> &contains, int n_x, int n_y, int n_z, R
 									particle2.z += translation[5];
 								}
 							}
-							if (abs(particle2.z - particle.z) > bulk_size_z / 2) { //more than one cell far
-								if (particle2.z > particle.z) { //move to neighbor cell
-									particle2.x -= translation[6];
-									particle2.y -= translation[7];
-									particle2.z -= translation[8];
-								}
-								else {
-									particle2.x += translation[6];
-									particle2.y += translation[7];
-									particle2.z += translation[8];
+							if (!surface) {
+								if (abs(particle2.z - particle.z) > bulk_size_z / 2) { //more than one cell far
+									if (particle2.z > particle.z) { //move to neighbor cell
+										particle2.x -= translation[6];
+										particle2.y -= translation[7];
+										particle2.z -= translation[8];
+									}
+									else {
+										particle2.x += translation[6];
+										particle2.y += translation[7];
+										particle2.z += translation[8];
+									}
 								}
 							}
 							if (toofar(particle, particle2, cutoff)) {
@@ -119,13 +191,11 @@ double full_energy(vector <vector<Atom>> &contains, int n_x, int n_y, int n_z, R
 							}
 
 							int choice = 0;
-							if (!impurity) { //turn on switcher
-								choice = 0;
-							} else if (particle.impurity && particle2.impurity) {
+							if (particle.impurity && particle2.impurity) {
 								choice = 2; //B-B
 							} else if (!particle.impurity && !particle2.impurity) {
 								choice = 0; //A-A
-							} else if (!particle.impurity || !particle2.impurity) {
+							} else {
 								choice = 1; //A-B
 							}
 
@@ -204,24 +274,20 @@ void dump(string filename, int size, vector <vector<Atom>> &contains) {
 	out.close();
 }
 
-int main(int argc, char* argv[]) {
+int convert(string filename, int n_x, int n_y, int n_z, double lattice_constant, vector <vector<Atom>> &out) {
 
+	out.resize(n_x * n_y * n_z);
 	vector<Atom> init;
 
-	ifstream fin(argv[1], ifstream::in);
+	ifstream fin(filename, ifstream::in);
 
 	if (!fin.is_open()) {
 		cout << "File can not be opened\n";
-		return 0;
+		exit(0);
 	}
 
-	string dumb;
 	int size = 0;
 	fin >> size;
-	
-	double bulk_size_x = 3 * lattice_constant;
-	double bulk_size_y = 3 * lattice_constant;
-	double bulk_size_z = 3 * lattice_constant;
 
 	while (fin.good()) {
 		Atom tmp;
@@ -233,21 +299,35 @@ int main(int argc, char* argv[]) {
 	}
 	fin.close();
 
+	for (unsigned int i = 0; i < init.size(); ++i) {
+		Atom tmp = init[i];
+		int x = int(tmp.x / lattice_constant); // step by lattice constant
+		int y = int(tmp.y / lattice_constant);
+		int z = int(tmp.z / lattice_constant);
+		out[z * n_y * n_x + y * n_x + x].push_back(tmp);
+	}
+	return size;
+}
+
+int main(int argc, char* argv[]) {
+
+	//cell size
 	int n_x = 3;
 	int n_y = 3;
 	int n_z = 3;
-	double step = lattice_constant;
 
-	vector <vector<Atom>> prev(n_x * n_y * n_z);
-	vector <vector<Atom>> curr(n_x * n_y * n_z);
-	for (unsigned int i = 0; i < init.size(); ++i) {
-		Atom tmp = init[i];
-		int x = int(tmp.x / step);
-		int y = int(tmp.y / step);
-		int z = int(tmp.z / step);
-		prev[z * n_y * n_x + y * n_x + x].push_back(tmp);
-		curr[z * n_y * n_x + y * n_x + x].push_back(tmp);
-	}
+	const double lattice_constant = 3.615;
+	const double cutoff = 1.7 * lattice_constant;
+	const double qsi = 0.8018993929636421;
+
+	//without impurities
+	vector <vector<Atom>> curr;
+	int size = convert(argv[1], n_x, n_y, n_z, lattice_constant, curr);
+	vector <vector<Atom>> prev = curr;
+
+	//with one impurity for E_sol
+	vector <vector<Atom>> imp;
+	convert(argv[2], n_x, n_y, n_z, lattice_constant, imp);
 
 	RGL potentials[3];
 
@@ -276,62 +356,30 @@ int main(int argc, char* argv[]) {
 	potentials[2].q = 2.139;
 	potentials[2].r0 = 2.378;
 
-	double translation[9];
-	//x
-	translation[0] = bulk_size_x;
-	translation[1] = 0;
-	translation[2] = 0;
-	//y
-	translation[3] = 0;
-	translation[4] = bulk_size_y;
-	translation[5] = 0;
-	//z
-	translation[6] = 0;
-	translation[7] = 0;
-	translation[8] = bulk_size_z;
+	Translation tr;
+	tr.lattice_constant = lattice_constant;
+	tr.make_it_pure();
 
-	double E_full = full_energy(prev, n_x, n_y, n_z, potentials, true, translation);
-	double E_full_without_impurity = full_energy(prev, n_x, n_y, n_z, potentials, false, translation); //for E_sol
+	double E_full = full_energy(prev, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
 	cout << "E_full = " << E_full << endl;
 	double E_coh = E_full / size;
 	cout << "E_coh = " << E_coh << endl;
 	cout << "alpha = ";
 	double alpha;
 	cin >> alpha;
-
 	double V_0 = lattice_constant * lattice_constant * lattice_constant * n_x * n_y * n_z;
 
 	//B////////////////////////////////////////////////////////////////////////////////////
 	stretch(curr, n_x, n_y, n_z, alpha);
-	//x
-	translation[0] = bulk_size_x * (1 + alpha);
-	translation[1] = 0;
-	translation[2] = 0;
-	//y
-	translation[3] = 0;
-	translation[4] = bulk_size_y * (1 + alpha);
-	translation[5] = 0;
-	//z
-	translation[6] = 0;
-	translation[7] = 0;
-	translation[8] = bulk_size_z * (1 + alpha);
-	double E_full_stretched = full_energy(curr, n_x, n_y, n_z, potentials, true, translation);
+	tr.alpha = alpha;
+	tr.make_it_B();
+	double E_full_stretched = full_energy(curr, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
 	curr = prev;
 
 	stretch(curr, n_x, n_y, n_z, -alpha);
-	//x
-	translation[0] = bulk_size_x * (1 - alpha);
-	translation[1] = 0;
-	translation[2] = 0;
-	//y
-	translation[3] = 0;
-	translation[4] = bulk_size_y * (1 - alpha);
-	translation[5] = 0;
-	//z
-	translation[6] = 0;
-	translation[7] = 0;
-	translation[8] = bulk_size_z * (1 - alpha);
-	double E_full_compressed = full_energy(curr, n_x, n_y, n_z, potentials, true, translation);
+	tr.alpha = -alpha;
+	tr.make_it_B();
+	double E_full_compressed = full_energy(curr, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
 	curr = prev;
 
 	double B = (2 * (E_full_stretched - 2 * E_full + E_full_compressed) * qsi) / (9 * V_0 * alpha * alpha);
@@ -339,67 +387,27 @@ int main(int argc, char* argv[]) {
 
 	//C11 C12//////////////////////////////////////////////////////////////////////////////
 	elastic_C11(curr, n_x, n_y, n_z, alpha);
-	//x
-	translation[0] = bulk_size_x * (1 + alpha);
-	translation[1] = 0;
-	translation[2] = 0;
-	//y
-	translation[3] = 0;
-	translation[4] = bulk_size_y * (1 + alpha);
-	translation[5] = 0;
-	//z
-	translation[6] = 0;
-	translation[7] = 0;
-	translation[8] = bulk_size_z;
-	double E_full_stretched_C11 = full_energy(curr, n_x, n_y, n_z, potentials, true, translation);
+	tr.alpha = alpha;
+	tr.make_it_C11();
+	double E_full_stretched_C11 = full_energy(curr, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
 	curr = prev;
 
 	elastic_C11(curr, n_x, n_y, n_z, -alpha);
-	//x
-	translation[0] = bulk_size_x * (1 - alpha);
-	translation[1] = 0;
-	translation[2] = 0;
-	//y
-	translation[3] = 0;
-	translation[4] = bulk_size_y * (1 - alpha);
-	translation[5] = 0;
-	//z
-	translation[6] = 0;
-	translation[7] = 0;
-	translation[8] = bulk_size_z;
-	double E_full_compressed_C11 = full_energy(curr, n_x, n_y, n_z, potentials, true, translation);
+	tr.alpha = -alpha;
+	tr.make_it_C11();
+	double E_full_compressed_C11 = full_energy(curr, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
 	curr = prev;
 
 	elastic_C12(curr, n_x, n_y, n_z, alpha);
-	//x
-	translation[0] = bulk_size_x * (1 + alpha);
-	translation[1] = 0;
-	translation[2] = 0;
-	//y
-	translation[3] = 0;
-	translation[4] = bulk_size_y * (1 - alpha);
-	translation[5] = 0;
-	//z
-	translation[6] = 0;
-	translation[7] = 0;
-	translation[8] = bulk_size_z;
-	double E_full_stretched_C12 = full_energy(curr, n_x, n_y, n_z, potentials, true, translation);
+	tr.alpha = alpha;
+	tr.make_it_C12();
+	double E_full_stretched_C12 = full_energy(curr, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
 	curr = prev;
 
 	elastic_C12(curr, n_x, n_y, n_z, -alpha);
-	//x
-	translation[0] = bulk_size_x * (1 - alpha);
-	translation[1] = 0;
-	translation[2] = 0;
-	//y
-	translation[3] = 0;
-	translation[4] = bulk_size_y * (1 + alpha);
-	translation[5] = 0;
-	//z
-	translation[6] = 0;
-	translation[7] = 0;
-	translation[8] = bulk_size_z;
-	double E_full_compressed_C12 = full_energy(curr, n_x, n_y, n_z, potentials, true, translation);
+	tr.alpha = -alpha;
+	tr.make_it_C12();
+	double E_full_compressed_C12 = full_energy(curr, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
 	curr = prev;
 
 	double C11 = ((E_full_stretched_C11 - 2 * E_full + E_full_compressed_C11) / (alpha * alpha) + (E_full_stretched_C12 - 2 * E_full + E_full_compressed_C12) / (alpha * alpha)) * qsi / (2 * V_0);
@@ -410,45 +418,28 @@ int main(int argc, char* argv[]) {
 	
 	//C44//////////////////////////////////////////////////////////////////////////////////
 	elastic_C44(curr, n_x, n_y, n_z, alpha);
-	//x
-	translation[0] = bulk_size_x;
-	translation[1] = alpha * bulk_size_x;
-	translation[2] = 0;
-	//y
-	translation[3] = alpha * bulk_size_y;
-	translation[4] = bulk_size_y;
-	translation[5] = 0;
-	//z
-	translation[6] = 0;
-	translation[7] = 0;
-	translation[8] = bulk_size_z / (1 - alpha * alpha);
-
-	double E_full_stretched_C44 = full_energy(curr, n_x, n_y, n_z, potentials, true, translation);
+	tr.alpha = alpha;
+	tr.make_it_C44();
+	double E_full_stretched_C44 = full_energy(curr, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
 	curr = prev;
 
 	elastic_C44(curr, n_x, n_y, n_z, -alpha);
-	//x
-	translation[0] = bulk_size_x;
-	translation[1] = -alpha * bulk_size_x;
-	translation[2] = 0;
-	//y
-	translation[3] = -alpha * bulk_size_y;
-	translation[4] = bulk_size_y;
-	translation[5] = 0;
-	//z
-	translation[6] = 0;
-	translation[7] = 0;
-	translation[8] = bulk_size_z / (1 - alpha * alpha);
-
-	double E_full_compressed_C44 = full_energy(curr, n_x, n_y, n_z, potentials, true, translation);
+	tr.alpha = -alpha;
+	tr.make_it_C44();
+	double E_full_compressed_C44 = full_energy(curr, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
 	curr = prev;
 
 	double C44 = ((E_full_stretched_C44 - 2 * E_full + E_full_compressed_C44)  * qsi) / (2 * V_0 * alpha * alpha);
 	cout << "C44 = " << C44 << endl;
 
-	//E_sol for Co
-	double E_sol = E_full - E_full_without_impurity + 4.386 + E_coh;
+	//E_sol
+	tr.make_it_pure();
+	double E_AB = full_energy(imp, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
+	double E_B = full_energy(curr, n_x, n_y, n_z, cutoff, potentials, tr.translation, false);
+	//E_coh of Co = -4.386
+	double E_sol = E_AB - E_B + 4.386 + E_coh;
 	cout << "E_sol = " << E_sol << endl;
+
 
 	return 0;
 }
